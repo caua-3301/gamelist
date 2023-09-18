0eb3bca29d760a2913baa4626b33417f1f70bab7
caua-3301 <134548536+caua-3301@users.noreply.github.com>
2023-09-17 22:28:45 -0300

    sugestoes para o js

---
+++ javascript/script-teste.js
@@ -0,0 +1,99 @@
+//elementos HTML
+let nome = document.querySelector("#nome");
+let descricao = document.querySelector("#descricao");
+let favorito = document.querySelector("#favorito");
+let btn_salvar = document.querySelector("#salvar");
+let lista = document.querySelector("#lista");
+
+//Verificar no localStorage se o item "banco" existe, caso não, é criado
+if (localStorage.getItem("banco") === null){
+    let gameList = [];
+    localStorage.setItem("banco", JSON.stringify(gameList));
+}
+
+//Pegando os valores armazenados em "banco"
+function getBanco() {
+    let banco = localStorage.getItem("banco");
+    return JSON.parse(banco);
+}
+
+//localStorage.removeItem("banco")
+
+//Adicionado as informações do jogo no item "banco"
+function addGameToData(infoGame) {
+    let gameInfo = infoGame
+    let prov = getBanco()
+    prov = [...prov, gameInfo]
+    
+    localStorage.setItem("banco", JSON.stringify(prov));
+}
+
+//Pegando os dados e jogando no array que vai para o "banco"
+function creatObject(id, nome, descricao, favorito) {
+    let auxArray = {id: id, nome: nome, descricao: descricao, favorito: favorito}
+    addGameToData(auxArray)
+}
+
+//Verificando se o item "banco" está vazio
+function emptyBanco() {
+    if (getBanco().length === 0){
+        return true;
+    }
+    return false;
+}
+
+//Criando um novo id com base no tamanho da meu item "banco", nunca se repetirá (possível problema com delete)
+function newId() {
+    if (!emptyBanco){
+        return 0;
+    }
+    return getBanco().length + 1;
+}
+
+//Carregando os dados existente que estão no LocalStorage
+function loadList() {
+    if (emptyBanco){
+        for (let games of getBanco()){
+            let item = document.createElement("div");
+            let marcado = games.favorito? `<p>favorito</p>`:  `<p></p>`;
+
+            //html do item que vai ser salvo na lista
+            item.innerHTML = 
+            `
+            <h1>${games.nome}</h1>
+            <p>${games.descricao}</p>
+    
+            `
+            + marcado
+            +`<button id='ide'>Deletar</button>`
+            +`<button>${games.id}</button>`;
+
+            lista.appendChild(item);
+        }
+    }
+}
+
+loadList()
+
+//Salvando elementos
+function salvar() {
+    let item = document.createElement("div");
+    let id = newId()
+    let marcado = favorito.checked? `<p>favorito</p>`:  `<p></p>`;
+
+        //html do item que vai ser salvo na lista
+        item.innerHTML = 
+        `
+        <h1>${nome.value}</h1>
+        <p>${descricao.value}</p>
+    
+        `
+        + marcado
+        +`<button id='ide'>Deletar</button>`
+        +`<button>${id}</button>`;
+        lista.appendChild(item);
+
+    creatObject( id, nome.value, descricao.value, favorito.checked)
+}
+
+btn_salvar.addEventListener("click", salvar)
