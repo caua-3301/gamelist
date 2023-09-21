ccbe05a7e0bdbbad36b8ca4c2e7f4154b6843495
caua-3301 <134548536+caua-3301@users.noreply.github.com>
2023-09-21 19:42:28 -0300

    adicionando o js na pagina

--- javascript/script.js
+++ javascript/script.js
@@ -1,107 +1,106 @@
-
-//elementos HTML
 let nome = document.querySelector("#nome");
 let descricao = document.querySelector("#descricao");
-let favorito = document.querySelector("#favorito");
-let btn_salvar = document.querySelector("#salvar");
-let lista = document.querySelector("#lista")
+let radiosim = document.querySelector("#radiosim");
+let radionao = document.querySelector("#radionao");
+let lista = document.querySelector("#lista");
 
-if (localStorage.getItem("banco")===null) {
-    let array = []
+let banco = JSON.parse(localStorage.getItem("banco"));
+
+//Verificando se o localStorage possui o item "banco", caso não possua, ele cria
+if (banco === null) {
+    let array = [];
     localStorage.setItem("banco", JSON.stringify(array));
+    banco = JSON.parse(localStorage.getItem("banco"));
 }
 
-function GetBanco () {
-    let banco = localStorage.getItem("banco");
-    return JSON.parse(banco);
+//A partir dessa funcao anonima a funcoes LoadList  pode criar elemento html de forma mais facil
+let elementoHTML = (id, nome, descricao, favorito) => {
+    return `
+            <li class="item-jogo ${favorito ? 'item-jogo-fav' : 'item-jogo'}" ">
+                <img src="/img/joystick-svgrepo-com 1.png" alt="Imagem de um joystick" id="joy-image">
+
+                <div id="info-game">
+                    <p id="saida-nome-jogo">${nome}</p>
+                    
+ 
+                    <p id="saida-descricao">${descricao}>
+                    </p>
+                </div>
+
+                <div id="container-icons">
+                    <span class="material-symbols-outlined trash" onclick="removeJogo(${id})">
+                        delete
+                    </span>
+                    <span class="material-symbols-outlined star ${favorito ? 'star-fav' : 'star'}" onclick="setFavorito(${id})"  >
+                        star
+                    </span>
+                </div>
+            </li>
+            `;
+
 }
 
-function SetBanco(array) {
-    localStorage.setItem("banco",JSON.stringify(array));
-}
-
-function EmptyBanco() {
-    if (GetBanco().lenght === 0) {
-        return true
-    }
-    else {return false}
-}
-
-
-//retorna um novo id diferente dos outros
-function newId () {
-    if (EmptyBanco()) {
-        return 1
-    }
-
-    else {
-        //pegando uma lista com todos os id
-        let list = GetBanco().map((item)=>{return item.id})
-        //pegando o maior id
-        let maior_id = Math.max(...list)
-        return maior_id+1
-    }
-}
-
-
-//carregar o dados existentes
-function LoadList(){
-    if (!EmptyBanco()) {
-        let k = 0;
-        let banco = GetBanco()
-        for (k=0; k< banco.length; k++) {
-            let item = document.createElement("div");
-
-            let marcado = banco[k].favorito? `<p>favorito</p>`:  `<p></p>`;
-
-            item.innerHTML = 
-            `
-                <h1>${banco[k].nome}</h1>
-                <p>${banco[k].descricao}</p>
-            
-            `
-            + marcado
-            +`<button>${banco[k].id}</button>`;
-            lista.appendChild(item);
-            }
-    }
-}
-//SetBanco([{id: 1, nome: "gta", descricao:"muito bom", favorito:true}])
-LoadList()
-
-function pushJogo(id, nome, descricao, favorito) {
-    let jogo = { id: id, nome:nome , descricao:descricao , favorito: favorito }
-    let list = GetBanco()
-    list = [...list, jogo]
-    SetBanco(list)
-}
-
-
-
-
-
-
-function Salvar() {
-    let item = document.createElement("div");
-    let id = newId()
-    let fvrt =  favorito.checked
-    marcado = fvrt? `<p>favorito</p>`:  `<p></p>`;
-
-    //html do item que vai ser salvo na lista
-    item.innerHTML = 
-    `
-        <h1>${nome.value}</h1>
-        <p>${descricao.value}</p>
+//Carregar o dados existentes
+function LoadList() {
     
-    `
-    + marcado
-    +`<button>${id}</button>`;
+    lista.innerHTML = "";
 
+    for (let game of banco) {
 
-    lista.appendChild(item);
+        let item = elementoHTML(game.id, game.nome, game.descricao, game.favorito);
 
-    pushJogo( id, nome.value, descricao.value, favorito.checked)
+        console.log(typeof(game.nome))
+
+        if (game.favorito) { //item lista
+            lista.innerHTML = item + lista.innerHTML;
+        }
+        else {                //lista  + item
+            lista.innerHTML = lista.innerHTML + item;
+        }
+    }
+    if (lista.innerHTML == "") {
+        lista.innerHTML = "A lista está vazia!";
+    }
 }
 
-btn_salvar.addEventListener("click", Salvar);
 
+//Função que removwe um jogo
+function removeJogo(id) {
+    let jogo = banco.filter(item => (item.id === id))[0];
+    let index = banco.indexOf(jogo);
+    banco.splice(index, 1);
+    localStorage.setItem("banco", JSON.stringify(banco));
+    LoadList();
+}
+
+//Adicionando a classe favorito ao jogo
+function setFavorito(id) {
+    let jogo = banco.filter(item => (item.id === id))[0];
+    let index = banco.indexOf(jogo);
+    jogo.favorito ? jogo.favorito = false : jogo.favorito = true;
+    banco[index] = jogo;
+    localStorage.setItem("banco", JSON.stringify(banco));
+    LoadList();
+}
+
+//Salvando o jogo e sua descrição
+function Salvar() {
+
+    let id = banco.length === 0 ? 1 : banco[banco.length - 1].id + 1;
+
+    let favorito = radiosim.checked; //true ou false
+
+    banco = [...banco, { id: id, nome: nome.value, descricao: descricao.value, favorito: favorito }];
+    localStorage.setItem("banco", JSON.stringify(banco));
+
+    LoadList();
+
+}
+
+//Ativando evento que salva o jogo
+let form = document.querySelector("#form");
+form.addEventListener("submit", () => Salvar())
+
+window.onload = () => nome.focus()
+
+LoadList();
